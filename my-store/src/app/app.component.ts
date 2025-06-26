ad69080ec90bd4cf2b6c16ec231feba7101f99c4
daniel-tickell <dtickell@gmail.com>
2025-06-25 21:46:08 -0700

    Layout and header working

--- my-store/src/app/app.component.ts
+++ my-store/src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
-import { NavbarComponent } from './navbar/navbar.component';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router'; // Import RouterOutlet
+import { HeaderComponent } from './layout/header/header.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent],
+  //imports: [HeaderComponent, CartComponent, ConfirmationComponent, ProductItemComponent, ProductItemDetailComponent, ProductListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- my-store/src/app/app.routes.ts
+++ my-store/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { CartComponent } from './components/cart/cart.component';
+import { ConfirmationComponent } from './components/confirmation/confirmation.component';
+import { ProductItemComponent } from './components/product-item/product-item.component';
+import { ProductItemDetailComponent } from './components/product-item-detail/product-item-detail.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'products', pathMatch: 'full' },
+  { path: 'products', component: ProductListComponent, title: 'Products' },
+  { path: 'product/:id', component: ProductItemDetailComponent, title: 'Product Detail' },
+  { path: 'cart', component: CartComponent, title: 'Shopping Cart' },
+  { path: 'confirmation', component: ConfirmationComponent, title: 'Order Confirmation' },
+  { path: '**', redirectTo: 'products' }
+];
